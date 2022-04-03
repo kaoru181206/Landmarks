a1e4aa966ac510e3968529fdf58b671b67c66363
kaoru.shiraga <karats24.kaoru@gmail.com>
2022-04-03 22:51:54 +0900

    第一回チュートリアル終了時点

--- Landmarks/CircleImage.swift
+++ Landmarks/CircleImage.swift
@@ -10,6 +10,10 @@ import SwiftUI
 struct CircleImage: View {
     var body: some View {
         Image("turtlerock")
+            .clipShape(Circle())
+            .overlay(
+                Circle().stroke(Color.white, lineWidth: 4))
+            .shadow(radius: 10)
     }
 }
 
