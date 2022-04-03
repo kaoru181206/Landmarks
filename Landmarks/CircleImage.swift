ebffc35e0d6c9035012b1cc7bc7369c6a7919d7a
kaoru.shiraga <karats24.kaoru@gmail.com>
2022-04-03 21:55:15 +0900

    Landmarks初回コミット 画像挿入時点

---
+++ Landmarks/CircleImage.swift
@@ -0,0 +1,20 @@
+//
+//  CircleImage.swift
+//  Landmarks
+//
+//  Created by 白髪馨 on 2022/04/02.
+//
+
+import SwiftUI
+
+struct CircleImage: View {
+    var body: some View {
+        Image("turtlerock")
+    }
+}
+
+struct CircleImage_Previews: PreviewProvider {
+    static var previews: some View {
+        CircleImage()
+    }
+}
